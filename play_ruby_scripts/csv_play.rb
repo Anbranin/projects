2548e98153bca841a0acb9c04c25bc9578d7d4d8
Anbranin <keichelman@gmail.com>
2019-06-28 13:38:19 -0400

    remove useless file'

--- play_ruby_scripts/csv_play.rb
+++ play_ruby_scripts/csv_play.rb
@@ -12,3 +12,17 @@ def parse_employee_data(csv)
     puts row.fetch('Last Name')
   end
 end
+
+def add_swipe_ids(csv)
+  CSV.foreach(csv, headers: true) do |row|
+    last_name = row.fetch('USER_LASTNAME')
+    first_name = row.fetch('USER_FIRSTNAME')
+    spire = row.fetch('USER_SPIREID')
+    user = User.find_by(last_name: last_name, first_name: first_name, spire_id: spire).present?
+    if user.present?
+      user.update_attributes swipe_id: row.fetch('USER_SWIPEID')
+    else
+      puts "User not found: #{last_name}, #{first_name}"
+    end
+  end
+end
